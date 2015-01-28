036a09cf5ef83656daa5137ce34afe6eed286323
Soufiane Ghzal <sghzal@gmail.com>
2015-01-28 02:23:40 +0100

    Update FaceFactory.php

--- lib/Face/Core/FaceFactory.php
+++ lib/Face/Core/FaceFactory.php
@@ -13,7 +13,7 @@ class FaceFactory {
     public static function buildFace($params,$className=null){
         if(is_array($params))
             return new EntityFace($params,$className);
-        else if(is_a($params, "Face\Core\FaceInterface"))
+        else if($param instanceof \Face\Core\EntityFace)
             return $params;
         else
             throw new \Exception("Invalid type for building a face");
