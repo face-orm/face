c28fb459701eecbfdd419020c784f7e2c28209e8
sghzal <sghzal@gmail.com>
2015-01-28 01:52:44 +0100

    Remove the useless function Orm::query

--- lib/Face/ORM.php
+++ lib/Face/ORM.php
@@ -14,28 +14,7 @@ use Face\Util\OOPUtils;
  * @author sghzal
  */
 abstract class ORM {
-    
-    /**
-     * Prepare a query
-     * @param string|Core\EntityFaceElement|\Face\Traits\EntityFaceTrait $what it can be a classname, a face object, or an entity that uses EntityFaceTrait
-     * @return \Face\Sql\Query\FQuery the query for the entity we asked
-     * @throws Exception\FacelessException if the entity we asked doesnt exist
-     */
-    public static function Query($what){
-        
-        if(is_string($what)){
-            $baseFace=  Core\FacePool::getFace($what);
-        }else if(is_a($what,'Face\Core\EntityFaceElement')){
-            $baseFace=$what;
-        }else if(OOPUtils::UsesTrait($what, 'Face\Traits\EntityFaceTrait')){
-            $baseFace=$what->getEntityFace();
-        }else{
-            throw new Exception\FacelessException("You asked a query for something that has no face");
-        }
-        
-        return new Sql\Query\SelectBuilder($baseFace);
-    }
-    
+
     /**
      * 
      * @param \Face\Sql\Query\FQuery $fQuery
